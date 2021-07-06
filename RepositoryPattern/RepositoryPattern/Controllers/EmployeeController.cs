6963ee97b9ba0337a63b333a72fb01703bb47a49
Bishwanath Dey Nayan <16103022@iubat.edu>
2021-07-06 23:29:59 +0600

    Repository Pattern

---
+++ RepositoryPattern/RepositoryPattern/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IDataRepository<Employee> _repo;
+
+        public EmployeeController(IDataRepository<Employee> repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Employee> employees = _repo.GetAll();
+            return Ok(employees);
+        }
+    }
+}
---
+++ RepositoryPattern/RepositoryPattern/DataManager/EmployeeManager.cs
@@ -0,0 +1,42 @@
+﻿using RepositoryPattern.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.DataManager
+{
+    public class EmployeeManager : IDataRepository<Employee>
+    {
+        readonly ApplicationContext _context;
+
+        public EmployeeManager(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public void Add(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public void update(Employee dbEntity, Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
